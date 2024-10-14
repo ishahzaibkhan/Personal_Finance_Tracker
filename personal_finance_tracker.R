b90c3ac4dd2ad7065f6c275f7cbe6dfc2f79d23f
Shahzaib Khan <ishahzaibkhan@gmail.com>
2024-10-14 15:57:33 +0500

    calculate monthly and total income, review spending by category

--- personal_finance_tracker.R
+++ personal_finance_tracker.R
@@ -1,5 +1,14 @@
 # importing libraries with tidyverse including ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, and forcats
 library(tidyverse)
+library(skimr)
 
 # importing personal finance file
-read.csv("")
+df <- read.csv("./Personal_Finance_Dataset.csv")
+
+# Getting the overview of the dataset
+skim(df)
+glimpse(df)
+
+# Creating the month feature
+df <- df %>%
+  mutate(month = month(Date))
