46d0477735d21600e432c1dcfe2530d54beac709
Shahzaib Khan <ishahzaibkhan@gmail.com>
2024-10-16 23:36:54 +0500

    add month and monthly income to df

--- personal_finance_tracker.R
+++ personal_finance_tracker.R
@@ -9,6 +9,29 @@ df <- read.csv("./Personal_Finance_Dataset.csv")
 skim(df)
 glimpse(df)
 
-# Creating the month feature
-df <- df %>%
-  mutate(month = month(Date))
+# Calculating the monthly income
+monthly_income <- df %>%
+  filter(month(Date) == 1) %>% 
+  summarise(Income = sum(Amount)) %>% 
+
+# Calculating the total income
+total_income <- df %>% 
+  summarise(total_income = sum(Amount))
+  
+# Reviewing spending by category
+df %>% 
+  group_by(Category) %>% 
+  summarise(spendings = sum(Amount))
+
+# Calculating the saving per month
+df %>%
+  group_by(month(Date)) %>% 
+  filter(Category == "Savings") %>% 
+  summarise(savings = sum(Amount)) %>% 
+
+total_savings <- df %>% 
+  filter(Category == "Savings") %>% 
+  summarise(total_savings = sum(Amount))
+
+total_spending <- total_income - total_savings
+
